4bd4737af9dbf6977c94e407a1b81d8ce0f95669
AdamMa <Adam_Ma520@outlook.com>
2022-04-04 22:01:37 +0800

    IDEA thrid commit

---
+++ src/main/java/com/atguigu/git/MyGitTests.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class MyGitTests {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+    }
+}

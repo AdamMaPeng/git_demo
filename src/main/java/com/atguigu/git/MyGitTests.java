581a1e57cb118d48bb0ee20a5506b6bab17540c7
AdamMa <Adam_Ma520@outlook.com>
2022-04-04 22:11:33 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/MyGitTests.java
+++ src/main/java/com/atguigu/git/MyGitTests.java
@@ -7,5 +7,7 @@ public class MyGitTests {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hello git5!");
+        System.out.println("hello hot-fix!");
+
     }
 }
